83ac7842473a18b9dfeb3334d23b640a912aff20
MiracleTom <tomhiddle01@gmail.com>
2021-12-01 22:46:45 +0900

    add div to mockup array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Container, Button, Modal, Box, Typography } from '@material-ui/core';
+import { Container, Button, Modal, Box, Typography, Card } from '@material-ui/core';
 import { AddCircleOutline, FolderOpen, Save } from '@material-ui/icons';
 import { useState } from 'react';
 import ColorPicker from 'material-ui-color-picker'
@@ -17,13 +17,23 @@ const modalStyle = {
   p: 4,
 }
 function App() {
+  const [divs, setDivs] = useState([
+    { id: 0, color: '#000' },
+    { id: 1, color: '#0F0' },
+    { id: 2, color: '#F00' },
+  ])
   const [isOpenAddModal, showOpenModal] = useState(false)
+  const [currentColor, setCurrentColor] = useState('')
 
   const openAddModal = () => {
     showOpenModal(true)
   }
   const addDiv = () => {
-    console.info("addDiv")
+    showOpenModal(false)
+    setDivs([...divs, {
+      id: divs.length,
+      color: currentColor
+    }])
   }
   const importDivs = () => {
     console.info("importDivs")
@@ -31,11 +41,25 @@ function App() {
   const exportDivs = () => {
     console.info("exportDivs")
   }
+  const renderDivs = () => {
+    return divs.map(div =>
+      <Card variant="outlined" key={div.id}>
+        <Box style={{ backgroundColor: div.color, height: '100px' }}>
+          <Typography variant="h6" component="h2">
+            {div.id}
+          </Typography>
+        </Box>
+      </Card>
+    )
+  }
+
   return (
     <Container>
       <Button variant="outlined" startIcon={<AddCircleOutline />} onClick={openAddModal}>Add Div</Button>
       <Button variant="outlined" startIcon={<FolderOpen />} onClick={importDivs}>Import from JSON</Button>
       <Button variant="outlined" startIcon={<Save />} onClick={exportDivs}>Export to JSON</Button>
+
+      {renderDivs()}
 
       <Modal
         open={isOpenAddModal}
@@ -44,14 +68,17 @@ function App() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={modalStyle}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Select Div Color
-          </Typography>
+          <Box>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Select Div Color
+            </Typography>
+            <Button variant="outlined" startIcon={<Save />} onClick={addDiv}>Add Div</Button>
+          </Box>
           <ColorPicker
             name='color'
             defaultValue='#000'
-            // value={this.state.color} - for controlled component
-            onChange={color => console.log(color)}
+            value={currentColor}
+            onChange={color => setCurrentColor(color)}
           />
         </Box>
       </Modal>
